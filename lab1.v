2a8b3bfdb8b7c22428139c7cf000130f60d25eea
YinChian <91kevin16@gmail.com>
2022-03-16 20:45:07 +0800

    初始狀態

---
+++ lab1.v
@@ -0,0 +1,17 @@
+module lab1(
+	input SMA_CLKIN,
+	input [3:0] KEY,
+	input CLOCK_50,
+	output reg [7:0] HEX0,
+	output reg [7:0] HEX1,
+	output reg [7:0] HEX2,
+	output reg [7:0] HEX3,
+	output reg [7:0] HEX4,
+	output reg [7:0] HEX5,
+	output reg [7:0] HEX6,
+	output reg [7:0] HEX7
+);
+
+	
+
+endmodule
---
+++ lab1_tb.v
@@ -0,0 +1,73 @@
+`timescale 1ns/1ns
+
+module lab1_tb;
+
+reg clk_50M;
+reg reset_n;  //low active
+reg key0;     //low active
+reg clk_sma;  // test clock
+reg [3:0] key;
+wire [6:0] hex0;
+wire [6:0] hex1;
+wire [6:0] hex2;
+wire [6:0] hex3;
+wire [6:0] hex4;
+wire [6:0] hex5;
+wire [6:0] hex6;
+wire [6:0] hex7;
+
+lab1 u1(
+                //////// CLOCK //////////
+                .CLOCK_50(clk_50M),
+                .KEY(key),
+                //////// SEG7 //////////
+                .HEX0(hex0),
+                .HEX1(hex1),
+                .HEX2(hex2),
+                .HEX3(hex3),
+                .HEX4(hex4),
+                .HEX5(hex5),
+                .HEX6(hex6),
+                .HEX7(hex7),
+                ///////////////////////
+                .SMA_CLKIN(clk_sma)
+                               
+        );  
+
+always
+  #10 clk_50M = ~clk_50M;
+  
+always
+  #31 clk_sma = ~clk_sma;  // 16129032Hz
+  
+//assign key[0] = reset_n;
+//assign key[1] = reset_n;
+//assign key[2] = reset_n;
+//assign key[3] = reset_n;
+
+initial
+  begin
+  reset_n = 0;  
+  clk_50M = 0 ;  
+  clk_sma = 0; 
+  key[0] = 1;
+
+  
+  #30 reset_n = 1;  
+  
+  #2_000_000
+  key[0] = 0;     // key0 press
+  #12_000_000
+  key[0] = 1;     // key0 release
+  
+  #100_000_000; // simualtion 100ms
+  $stop;
+  end
+  
+initial
+  begin
+  $monitor("time=%3d,reset_n=%d,key0=%d,hex0=%x hex1=%x hex2=%x hex3=%x hex4=%x hex5=%x hex6=%x hex7=%x",$time,reset_n,key0,hex0,hex1,hex2,hex3,hex4,hex5,hex6,hex7);
+  end
+  
+  
+endmodule
