2e6df441c7f64676f0ac0f2bf9a68dc93e10433e
YinChian <91kevin16@gmail.com>
2022-03-16 21:10:03 +0800

    僅有除頻、環境設計完畢

---
+++ Clock_Counter.v
@@ -0,0 +1,11 @@
+module Clock_Counter(
+	input CLK_50,
+	input oneHz,
+	input reset,
+	input testSignal,
+	output reg [6:0]
+);
+
+	always(posedge CLK_50)
+	
+endmodule
--- lab1.v
+++ lab1.v
@@ -2,16 +2,20 @@ module lab1(
 	input SMA_CLKIN,
 	input [3:0] KEY,
 	input CLOCK_50,
-	output reg [7:0] HEX0,
-	output reg [7:0] HEX1,
-	output reg [7:0] HEX2,
-	output reg [7:0] HEX3,
-	output reg [7:0] HEX4,
-	output reg [7:0] HEX5,
-	output reg [7:0] HEX6,
-	output reg [7:0] HEX7
+	output reg [6:0] HEX0,
+	output reg [6:0] HEX1,
+	output reg [6:0] HEX2,
+	output reg [6:0] HEX3,
+	output reg [6:0] HEX4,
+	output reg [6:0] HEX5,
+	output reg [6:0] HEX6,
+	output reg [6:0] HEX7
 );
-
+	wire reset = KEY[0];
+	
+	wire oneHz;
+	mod_1sec(CLOCK_50,reset,oneHz);
+	
 	
 
 endmodule
